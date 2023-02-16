793a78164c3190035382691a0ebc0036d9e7c6cb
Micheal David <seawajonlineail.com>
2023-02-16 09:31:20 -0800

    did a task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-/* This function prints a statement to the console */
+/**
+* main - Entry point
+*
+* Description: 'This function prints a statement'
+*
+* Return: Always 0 on Success
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
