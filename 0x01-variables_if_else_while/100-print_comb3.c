5184aa137ef9dd311e2452874351f8bcf9074b07
Micheal David <seawajonlineail.com>
2023-02-17 10:10:49 -0800

    did a task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -20,7 +20,11 @@ int main(void)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-			if ((i != 8) && (j != 9))
+			if ((i == 8) && (j == 9))
+			{
+				break;
+			}
+			else
 			{
 				putchar(',');
 				putchar(' ');
