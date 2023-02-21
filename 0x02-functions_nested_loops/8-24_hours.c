eae49e5c672d43329bbee5fe6fcb42b07fb3b652
Micheal David <seawajonline@gmail.com>
2023-02-21 22:10:11 +0000

    Functions Variables

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -22,8 +22,10 @@ void jack_bauer(void)
 					_putchar(':');
 					_putchar(k + '0');
 					_putchar(l + '0');
-					if (((i == 2) && (j == 3)) && ((k == 5) && (l == 9))
-							break;
+					if (((i == 2) && (j == 3)) && ((k == 5) && (l == 9)))
+					{
+						break;
+					}
 					_putchar('\n');
 				}
 			}
