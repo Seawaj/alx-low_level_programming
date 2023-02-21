a43abbe77bdc52dea7ed42253efa9a1648c2f7bd
Micheal David <seawajonline@gmail.com>
2023-02-21 19:17:37 +0000

    Functions Variables

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - the function
+ *
+ * @c: 'Takes any integer (c)'
+ *
+ * Return: 1 Always (Success)
+ */
+int int _isalpha(int c)
+{
+	if (isalpha(c) == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
