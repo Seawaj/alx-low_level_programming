0564d93d0a4862b1d048f0306925e3eadb9aa37c
Micheal David <seawajonline@gmail.com>
2023-02-21 18:34:22 +0000

    Functions Variables

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - the function
+ *
+ * @c: 'Takes any integer (c)'
+ *
+ * Return: 1 Always (Success)
+ */
+int _islower(int c)
+{
+	return (islower(c));
+}
