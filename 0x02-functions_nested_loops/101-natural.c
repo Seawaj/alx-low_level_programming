2f954b7776b9df2ad85a2592969fdfcff536d634
Micheal David <seawajonline@gmail.com>
2023-02-22 02:21:29 +0000

    Functions Variables

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	j = 0;
+	for (i = 3; i < 1024; i++)
+	{
+		if (((i % 3) == 0) || ((i % 5) == 0))
+		{
+			j += i
+		}
+	}
+	printf("%d\n", j);
+	return (0);
+}
