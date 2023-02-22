051097f7cd6dc2a901829f304fe2a269d657036b
Micheal David <seawajonline@gmail.com>
2023-02-22 02:06:16 +0000

    Functions Variables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,30 +12,31 @@ void print_times_table(int n)
 
 	if ((n <= 15) || (n >= 0))
 	{
-		int i, j, k;
+		int i, j, k, l;
 
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
 				k = j * i;
-				if (k >= 10)
+				if (k >= 100)
 				{
-					_putchar((k / 10) + '0');
+					l = k / 10;
+					_putchar((l / 10) + '0');
+					_putchar((l % 10) + '0');
 					_putchar((k % 10) + '0');
 				}
+				else if (k >= 10)
+				{_putchar((k / 10) + '0'), _putchar((k % 10) + '0'); }
 				else
 				{_putchar(k + '0'); }
 				if (j == n)
 				{break; }
 				_putchar(',');
 				if ((((j + 1) * i) >= 10) && (((j + 1) * i) < 100))
-				{_putchar(' '); }
+				{_putchar(' '), _putchar(' '); }
 				else if (((j + 1) * i) >= 100)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
+				{_putchar(' '); }
 				else
 				{
 					_putchar(' ');
