7ffdf2fe703ba4ef41549016c1b4ed5edd80af50
Micheal David <seawajonline@gmail.com>
2023-02-23 00:21:58 +0000

    Functions Variables

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -11,3 +11,5 @@ int add(int, int);
 void print_to_98(int n);
 void times_table(void);
 void print_times_table(int n);
+void print_remaining_days(int month, int day, int year);
+int largest_number(int a, int b, int c);
