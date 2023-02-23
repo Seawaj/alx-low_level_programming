aa947f5781a16a7cbb813cecd68fdd0962bca0c6
Micheal David <seawajonline@gmail.com>
2023-02-23 01:06:47 +0000

    Functions Variables

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int a, b, c, sum;
+
+	a = 1;
+	b = 2;
+	c = 0;
+	sum = 0;
+	printf("%lu, %lu", a, b);
+	while (c <= 4000000)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (c > 4000000)
+		{break; }
+		if (c % 2 == 0)
+		{sum += c; }
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
