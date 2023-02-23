ec58f844c96e19459b67c92e554420295bf7b244
Micheal David <seawajonline@gmail.com>
2023-02-23 09:39:49 +0000

    Functions Variables

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - checks for uppercase letters
+ *
+ * @c: any integer
+ *
+ * Return: 0 on Success
+ */
+int _isupper(int c)
+{
+	return isupper(c) == 0 ? 0 : 1;
+}
