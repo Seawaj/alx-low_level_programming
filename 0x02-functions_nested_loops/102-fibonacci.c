3d83775f46565df03dd73ded1b674c22420007ef
Micheal David <seawajonline@gmail.com>
2023-02-23 00:41:10 +0000

    Functions Variables

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,22 +7,21 @@
  */
 int main(void)
 {
-	int a, b, c, count;
+	unsigned long int a, b, c, count;
 
 	a = 1;
 	b = 2;
 	count = 0;
 	printf("%d %d, ", a, b);
-	while (b <= 20365011074 && count < 48)
+	while (count < 48)
 	{
 		c = a + b;
 		if (c <= 0)
 		{break; }
-		printf("%d, ", c);
+		printf("%lu, ", c);
 		a = b;
 		b = c;
 		count++;
 	}
-	printf("%d\n", a + b);
 	return (0);
 }
