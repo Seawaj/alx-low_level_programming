7884f4cb53e3f5612b51b7ec46906390f137c80b
Micheal David <seawajonlineail.com>
2023-02-27 14:04:02 -0800

    Functions Variables

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,21 @@
  */
 void rev_string(char *s)
 {
-	int i, j;
+	int i, j, k;
+	
+	k = strlen(s) + 1;
+
+	char a[k];
 
 	j = 0;
 
-	for (i = strlen(s) - 1; i >= 0; i--)
+	for (i = k - 1;  i >= 0; i--)
 	{
-		s[j] = s[i];
+		a[i] = s[i];
+	}
+	for (i = k - 2; i >= 0; i--)
+	{
+		s[j] = a[i];
 		j++;
 	}
 }
