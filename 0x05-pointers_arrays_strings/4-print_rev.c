46fc481896eb62d235b6a07a6440f9b92de87e31
Micheal David <seawajonlineail.com>
2023-02-27 12:09:54 -0800

    Functions Variables

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - Prints string in reverse order
+ *
+ * @s: any string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	puts(strrev(s));
+}
