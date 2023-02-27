763e0a16d2be7bf72d97ca572d85752969e343ff
Micheal David <seawajonlineail.com>
2023-02-27 15:39:00 -0800

    Functions Variables

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - copies string
+ *
+ * @dest: destination
+ * @src: string to be copied
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
