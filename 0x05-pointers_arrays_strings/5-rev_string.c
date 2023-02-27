ba0e16fd29e784f043c495ef1861796dc22d4e19
Micheal David <seawajonlineail.com>
2023-02-27 12:29:29 -0800

    Functions Variables

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: any string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char a[strlen(s)];
+	int j;
+
+	j = 0;
+
+	for (i = strlen(s) - 1; i >= 0; i--)
+	{
+		a[j] = s[i];
+		j++;
+	}
+	*s = *a;
+}
