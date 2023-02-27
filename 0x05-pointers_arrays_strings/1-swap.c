05c2c4d762e32a1df094fd44a1c32fb28c035fc5
Micheal David <seawajonlineail.com>
2023-02-27 11:23:26 -0800

    Functions Variables

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - Function that swaps two values
+ *
+ * @a: any pointer
+ * @b: anypointer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c, d;
+
+	c = *a;
+	d = *b;
+	*a = c;
+	*b = d;
+}
