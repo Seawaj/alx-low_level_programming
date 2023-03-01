d6cd0596c755418d0997ac544a02703591cb2ae2
Micheal David <seawajonlineail.com>
2023-03-01 15:04:51 -0800

    Functions Variables

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,14 +9,13 @@
  */
 char *cap_string(char *str)
 {
-	int i;
-	bool j;
+	int i, j;
 
-	j = ((str[i - 1] == ' ') || (str[i - 1] == '.'));
 	if (str[0] >= 'a' && str[0] <= 'z')
 		str[0] = str[0] - 32;
 	for (i = 1; str[i] != '\0'; i++)
 	{
+		j = ((str[i - 1] == ' ') || (str[i - 1] == '.')) ? 1 : 0;
 		if (j && (str[i] >= 'a' && str[i] <= 'z'))
 		{
 			str[i] = str[i] - 32;
