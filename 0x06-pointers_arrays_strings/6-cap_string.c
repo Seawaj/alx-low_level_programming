b984f5be664103f63779dbf6b37dc80837f99790
Micheal David <seawajonlineail.com>
2023-03-01 14:55:39 -0800

    Functions Variables

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - Changes string word from lowercase to uppercase
+ *
+ * @str: The string to be changed
+ *
+ * Return: Uppercase String word
+ */
+char *cap_string(char *str)
+{
+	int i;
+	bool j;
+
+	j = ((str[i - 1] == ' ') || (str[i - 1] == '.'));
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] = str[0] - 32;
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		if (j && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
