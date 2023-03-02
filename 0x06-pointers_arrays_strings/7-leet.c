39ee265bcd8f0fb40798294e9c9d848341d5ad0a
Micheal David <seawajonlineail.com>
2023-03-01 16:35:48 -0800

    Functions Variables

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
  * leet - Encode String
@@ -9,34 +12,26 @@
  */
 char *leet(char *str)
 {
-	int i;
+	int i, j;
+	char *result, c;
 
+	result = (char *)malloc(strlen(str) + 1);
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((str[i] == 'a') || (str[i] == 'A'))
+		result[i] = str[i];
+	}
+
+	result[i] = '\0';
+
+	for (i = 0, j = 0; str[i] != '\0'; i++, j++)
+	{
+		c = tolower(str[i]);
+
+		if (c == 'a' || c == 'e' || c == 'o' || c == 't' || c == 'l')
 		{
-			str[i] = '4';
-		}
-		else if ((str[i] == 'e') || (str[i] == 'E'))
-		{
-			str[i] = '3';
-		}
-		else if ((str[i] == 'o') || (str[i] == 'O'))
-		{
-			str[i] = '0';
-		}
-		else if ((str[i] == 't') || (str[i] == 'T'))
-		{
-			str[i] = '7';
-		}
-		else if ((str[i] == 'l') || (str[i] == 'L'))
-		{
-			str[i] = '1';
-		}
-		else
-		{
-			continue;
+			result[j] = '0' + (c == 'a') * 4 + (c == 'e') * 3 + (c == 'o') * 0 + (c == 't') * 7 + (c == 'l') * 1;
 		}
 	}
-	return (str);
+
+	return (result);
 }
