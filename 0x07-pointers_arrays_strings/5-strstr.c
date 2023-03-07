bba6c7405ef86dbb29698e7de9e4dcacb4735e8d
Micheal David <seawajonlineail.com>
2023-03-07 15:29:57 -0800

    Functions Variables

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strstr - finds the first occurrence of a substring in a string
+ *
+ * @haystack: string
+ * @needle: sebstring
+ *
+ * Return: A pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+}
