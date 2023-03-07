79f28504573175e470fd2f70ed73c182c49307bc
Micheal David <seawajonlineail.com>
2023-03-07 12:54:14 -0800

    Functions Variables

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _memset - function fills the first n bytes of the memory area pointed to by s with the constant byte b
+ *
+ * @s: pointer
+ * @b: any char
+ * @n: unsigned int
+ *
+ * Return: A pointer of type char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (memset(s, b, n*sizeof(s));
+}
