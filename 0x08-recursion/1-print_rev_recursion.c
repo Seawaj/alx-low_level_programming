7e415f1d3cc7460b42b7ba444db965abacaf15fc
Micheal David <seawajonlineail.com>
2023-03-09 09:52:08 -0800

    Functions Variables

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _print_rev_recursion - prints function in reverse
@@ -13,6 +12,6 @@ void _print_rev_recursion(char *s)
 	if (*s)
 	{
 		_putchar(*s);
-		_print_rev_recursion(s + 1)
+		_print_rev_recursion(s + 1);
 	}
 }
