02dd5820219b497947314cf96bc6563e5009bd83
Micheal David <seawajonlineail.com>
2023-03-09 15:25:20 -0800

    Functions Variables

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - finds square root of a number
  * _sqrt - additional funtion to find the real square root
  *
  * @n: any number
@@ -13,15 +12,23 @@
 int _sqrt(int n, int i)
 {
 	if (i * i == n) 
-		return (n);
+		return (i);
 	else if (i * i > n)
 		return (-1);
 	else
 		return (_sqrt(n, i + 1));
 }
+
+/**
+ * _sqrt_recursion - funtion to find the real square root
+ *
+ * @n: any number
+ *
+ * Return: integer
+ */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
+	if (n <= 0)
 		return (-1);
 	return (_sqrt(n, 0));
 }
