48ae7a9f98e8a99a77241df2af606665dea1508a
Micheal David <seawajonlineail.com>
2023-03-09 14:12:50 -0800

    Functions Variables

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _pow_recursion - finds the power of any number
+ *
+ * @x: any number
+ * @y: any number
+ *
+ * Return: integer
+ */
+int _pow_recursion(int x, int y)
+{
+	return (pow(x, y));
+}
