19293cd20a2aa67834243f47674c05642118f1af
Micheal David <seawajonlineail.com>
2023-03-09 10:00:20 -0800

    Functions Variables

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - finds the length of a string
+ *
+ * @s: any string
+ *
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
