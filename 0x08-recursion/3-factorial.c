32c971fbfc626c27fc459b8ca11421f150ac802a
Micheal David <seawajonlineail.com>
2023-03-09 10:14:49 -0800

    Functions Variables

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - finds the factorial of an integer
+ *
+ * @n: any integer
+ *
+ * Return: int
+ */
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (n * factorial(n - 1));
+}
