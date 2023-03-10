6ca0380cd009c07a813bbfe5d02d1c4de33ff365
Micheal David <seawajonlineail.com>
2023-03-09 16:36:53 -0800

    Functions Variables

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _len - prints function in reverse
+ *
+ * @s: any string
+ *
+ * Return: integer
+ */
+int _len (char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (_len(s + 1) + 1);
+}
+
+/**
+ * real_pal - prints function in reverse
+ *
+ * @s: any string
+ * @i: any integer
+ * @j: any integer
+ *
+ * Return: integer
+ */
+int real_pal(char *s, int i, int j)
+{
+	if (*(s + j - 1) != *(s + i))
+		return (0);
+	else if (i >= len)
+		return (1);
+	else
+		return (real_pal(s, i + 1, j - 1));
+}
+
+/**
+ * is_palindrome - checks for palindrome
+ *
+ * @s: any string
+ *
+ * Return: int
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (0);
+	else
+		return (real_pal(s, 0, _len(s)));
+}
