01a245e1430ef022a1e4c4176976254413307b47
Micheal David <seawajonlineail.com>
2023-03-10 23:52:32 -0800

    Functions Variables

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: Holds number of entered arguments
+ * @argv: An array of entered arguments
+ *
+ * Return: ALways 0 on Success
+ */
+int __attribute__ ((unused)) main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
