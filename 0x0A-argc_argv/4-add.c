aac48e2fa27d6034e693bf4100c7c995732e93c5
Micheal David <seawajonlineail.com>
2023-03-11 02:20:01 -0800

    Functions Variables

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,35 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 /**
- * main - Entry point
- *
- * @argc: Holds number of entered arguments
- * @argv: An array of entered arguments
- *
- * Return: ALways 0 on Success
- */
+* check_num - checks if string there are digit
+* @str: array str
+*
+* Return: Always 0 (Success)
+*/
+int check_num(char *str)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < strlen(str))
+	{
+		if (!isdigit(str[i]))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+/**
+* main - Entry point
+*
+* @argc: number of arguments
+* @argv: Arguments
+*
+* Return: Always 0 (Success)
+*/
 int main(int argc, char *argv[])
 {
-	int add, i;
+	int i, j, sum;
 
-	add = 0;
-	if (argc == 1)
+	sum = 0;
+	i = 1;
+	while (i < argc)
 	{
-		printf("%d\n", add);
-		return (0);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		if (isdigit(atoi(argv[i])))
-			add += atoi(argv[i]);
+		if (check_num(argv[i]))
+		{
+			j = atoi(argv[i]);
+			sum += j;
+		}
 		else
 		{
 			printf("Error\n");
 			return (1);
 		}
+		i++;
 	}
-	printf("%d\n", add);
+	printf("%d\n", sum);
 	return (0);
 }
