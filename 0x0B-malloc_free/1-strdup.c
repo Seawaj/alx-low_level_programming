d15c95394909e1c38af7ae23d51841a87bbe3254
Micheal David <seawajonline@gmail.com>
2023-03-14 09:52:33 +0000

    Functions Variables

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer toa new string
+ *
+ * @str: The old string
+ *
+ * Return: Pointer to a new string else NULL
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i;
+
+	ptr = malloc(strlen(str) * sizeof(str));
+	if ((str == NULL) || (ptr == NULL))
+		return (NULL);
+	for (i = 0; i < strlen(str); i++)
+		ptr[i] = str[i];
+	return (ptr);
+}
