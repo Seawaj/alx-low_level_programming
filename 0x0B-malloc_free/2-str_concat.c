c496fce9cdcdada1e87b3da09a4b4c13c34edf46
Micheal David <seawajonline@gmail.com>
2023-03-14 11:12:52 +0000

    Functions Variables

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,17 +13,19 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	unsigned int i;
+	unsigned int i, j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	strcat(s1, s2);
-	ptr = malloc((strlen(s1) + 1) * sizeof(char));
+	ptr = malloc((strlen(s1) + strlen(s2)) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
+	j = strlen(s1);
 	for (i = 0; s1[i]; i++)
 		ptr[i] = s1[i];
+	for (i = 0; s2[i]; i++)
+		ptr[strlen(ptr)] = s2[i];
 	return (ptr);
 }
