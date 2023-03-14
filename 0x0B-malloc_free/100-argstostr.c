bed84f2fc65fe46ac323a18dd513ac17646c611b
Micheal David <seawajonline@gmail.com>
2023-03-14 15:51:46 +0000

    Functions Variables

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments
+ *
+ * @ac: number of arguments passed
+ * @av: arguments
+ *
+ * Return: poiter to concatenated arguments passed
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	unsigned int i, j;
+
+	if ((ac == 0) || (av == NULL))
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		if (av[i] == NULL)
+			return (NULL);
+		for (j = 0; j < strlen(av[i]); j++)
+			str[strlen(str)] = av[i][j];
+		str[strlen(str)] = '\n';
+	}
+}
