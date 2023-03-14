65a1420d103ba00c7466e6ce8a4f83979feb5496
Micheal David <seawajonline@gmail.com>
2023-03-14 09:39:25 +0000

    Functions Variables

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,15 +9,20 @@
  *
  * Return: A pointer to char array else NULL
  */
-char *create_array(unsigned int size, __attribute__((unused))char c)
+char *create_array(unsigned int size, char c)
 {
 	char *ptr;
+	int i;
 
 	if (size == 0)
 		return (NULL);
 
-	ptr = malloc(size * sizeof(char));
+	ptr = malloc(size * sizeof(c));
 	if (ptr != NULL)
+	{
+		for (i = 0; i < size; i++)
+			ptr[i] = c
 		return (ptr);
+	}
 	return (NULL);
 }
