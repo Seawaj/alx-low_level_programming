c290fdcc920eb717837d5429d5b296d437d9a2d0
Micheal David <seawajonline@gmail.com>
2023-03-14 15:25:36 +0000

    Functions Variables

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid
+ *
+ * @grid: The grid to be freed
+ * @height: hieght of the grid
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
