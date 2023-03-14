3e79d55f99c22c11cd49e943f913027463f46822
Micheal David <seawajonline@gmail.com>
2023-03-14 10:21:36 +0000

    Functions Variables

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,15 +17,11 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	j = 0;
-	while (str[j] != '\0')
-		j++;
-
-	ptr = malloc((j + 1) * sizeof(str));
+	ptr = malloc(strlen(str) * sizeof(str));
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < j; i++)
+	for (i = 0; str[i]; i++)
 		ptr[i] = str[i];
 	return (ptr);
 }
