1018a1c308a9014c7593126f6959fda97bb80efc
Micheal David <seawajonline@gmail.com>
2023-03-17 16:35:05 +0000

    Functions Variables

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,21 +13,19 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	unsigned int i;
+	size_t len1, len2;
+	char *result;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	if (strlen(s2) < n)
-		n = strlen(s2);
-	ptr = malloc(strlen(s1) + n);
-	if (ptr == NULL)
-		return (NULL);
-	for (i = 0; s1[i]; i++)
-		ptr[i] = s1[i];
-	for (i = 0; i < n; i++)
-		ptr[strlen(ptr)] = s2[i];
-	return (ptr);
+	if (s1 == NULL) s1 = "";
+	if (s2 == NULL) s2 = "";
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	if (n >= len2) n = len2;
+	result = malloc(len1 + n + 1);
+	if (result == NULL) return NULL;
+	memcpy(result, s1, len1);
+	memcpy(result + len1, s2, n);
+	result[len1 + n] = '\0';
+	return (result);
 }
---
+++ 0x0C-more_malloc_free/jk.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * string_nconcat - concat's two strings in nth char
+ *
+ * @s1: destination string
+ * @s2: string to be concat to s2
+ * @n: any integer
+ *
+ * Return: A pointer or NULL
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (strlen(s2) < n)
+		n = strlen(s2);
+	ptr = malloc(strlen(s1) + n);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; s1[i]; i++)
+		ptr[i] = s1[i];
+	for (i = 0; i < n; i++)
+		ptr[strlen(ptr)] = s2[i];
+	return (ptr);
+}
