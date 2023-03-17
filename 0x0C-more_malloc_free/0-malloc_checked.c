2b4171c593fa03243f3f0747df4525796b43a420
Micheal David <seawajonline@gmail.com>
2023-03-17 00:15:54 +0000

    Functions Variables

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - Returns a pointer to an allocated memory
+ *
+ * @b: units required
+ *
+ * Return: A pointer or normal exit code
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *ptr;
+
+	ptr = malloc(b * sizeof(unsigned int));
+	if (ptr == NULL)
+		exit(98);
+	else
+		return (ptr);
+}
