e808d847da76b133bb1b27dc091697aa1b5482ba
Micheal David <seawajonline@gmail.com>
2023-03-17 15:54:07 +0000

    Functions Variables

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,6 +18,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if ((n == NULL) || (n < 0))
 		return (NULL);
+	if (s1 == NULL)
+		s1 == "";
+	if (s2 == NULL)
+		s2 = "";
 	if (strlen(s2) < n)
 		n = strlen(s2);
 	strncat(s1, s2, n);
