57f0185d567acbbd272558e4779a716f65807490
Micheal David <seawajonline@gmail.com>
2023-03-17 16:12:43 +0000

    Functions Variables

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; s1[i]; i++)
 		ptr[i] = s1[i];
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 		ptr[strlen(ptr)] = s2[i];
 	return (ptr);
 }
