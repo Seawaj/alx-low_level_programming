cf7d739b9b10ae4293f3d830fd16681988d7f552
Micheal David <seawajonline@gmail.com>
2023-03-21 02:33:29 +0000

    Functions Variables

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - struct for dog
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
