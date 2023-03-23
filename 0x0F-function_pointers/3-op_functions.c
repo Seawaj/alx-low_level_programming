16a09fcdbd6729b6c2a57bec415113d1a7c6e63f
Micheal David <seawajonline@gmail.com>
2023-03-23 04:19:31 +0000

    Functions Variables

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - finds difference
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - finds the product of two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - finds the division of two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - finds the modulus of two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: modulus of a by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
