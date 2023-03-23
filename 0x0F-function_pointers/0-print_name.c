245bed337581d7478746314095ffb341fb9fd304
Micheal David <seawajonline@gmail.com>
2023-03-23 03:15:48 +0000

    Functions Variables

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_name - Prints a name
+ *
+ * @name: Name to be printed
+ * @f: ponter to a function
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
