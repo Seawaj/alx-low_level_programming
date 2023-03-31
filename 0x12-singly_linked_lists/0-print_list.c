e3db5bab2d858820b5c8f7b9b2a644b590c55cf4
Seawaj <seawajonline@gmail.com>
2023-03-30 18:34:55 -0700

    Functions Variables

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,8 +10,9 @@
 */
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	size_t i;
 
+	i = 0;
 	while (h)
 	{
 		if (!h->str)
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* list_len - returns the number of elements in a linked list
+*
+* @h: pointer to the linked list
+*
+* Return: number of elements in h
+*/
+size_t list_len(const list_t *h)
+{
+	size_t i;
+
+	i = 0;
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
+
