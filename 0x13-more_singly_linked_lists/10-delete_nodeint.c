bd3388fd66240426d601ea3ea821ed3b154b0baa
Seawaj <seawajonline@gmail.com>
2023-04-04 18:20:11 -0700

    Functions Variables

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i;
 
 	tmp = *head;
-	str = NULL
+	str = NULL;
 	if (*head == NULL)
 		return (-1);
 	if (index == 0)
