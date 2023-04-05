4a1232f581ffd503286cc9c8516942d4b76ee473
Seawaj <seawajonline@gmail.com>
2023-04-04 17:30:26 -0700

    Functions Variables

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* free_listint2 - frees a listint_t list and sets head to NULL
+*
+* @head: pointer to the linked list
+*
+* Return: void
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	*head = NULL;
+}
